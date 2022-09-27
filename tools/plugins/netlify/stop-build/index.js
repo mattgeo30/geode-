e21dd1ae8c2dfc1a5d6452dfb6297b4e2c207d13
matt <mattgeo30@gmail.com>
2022-09-27 00:08:42 -0400

    Anotha one

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -1,5 +1,12 @@
 module.exports = {
-    onBuild: () => {
+    onBuild: ({utils}) => {
       console.log('Hello world from the plugin!');
+      const currentProject = 'geode';
+      const projectHasChanged = false;
+      if (!projectHasChanged) {
+        utils.build.cancelBuild(
+          `Build was cancelled because ${currentProject} was not affected by the latest changes`
+        );
+      }
     }
   };
